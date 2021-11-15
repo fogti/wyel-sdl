fee5a2bf432601a52a43925c56e883b8057debb0
zseri <zseri.devel@ytrizja.de>
2021-11-15 20:02:46 +0100

    port of rect.* to rust is trivial

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod font;
 mod fs;
 mod state_guard::StateGuard;
 
+/// `is_hit` is replaced by sdl2::rect::Rect::has_intersection
+
 fn main() {
     println!("Hello, world!");
 }
--- src/rect.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <SDL.h>
-bool operator==(const SDL_Rect& a, const SDL_Rect& b) noexcept;
-bool operator!=(const SDL_Rect& a, const SDL_Rect& b) noexcept;
-bool is_hit(const SDL_Rect &a, const SDL_Rect &b) noexcept;
