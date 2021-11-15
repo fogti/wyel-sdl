84a825900ebc61759f4c000750d847040a86bfe3
zseri <zseri.devel@ytrizja.de>
2021-11-15 20:03:42 +0100

    deal with error message printing later

--- src/error.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-void errmsg(const std::string &msg);
-void sdl_xxx_errmsg(const char *const subsystem, const char *const fn);
-#define sdl_errmsg(fn) sdl_xxx_errmsg("SDL", fn);
-#define ttf_errmsg(fn) sdl_xxx_errmsg("TTF", fn);
